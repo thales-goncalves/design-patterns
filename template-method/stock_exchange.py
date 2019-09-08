1995207f79798210f9b9ff7432f2f6e586a7191a
Thales Goncalves <thales.goncalves@inatel.br>
2019-09-08 20:27:39 -0300

    Fix template method

--- template-method/stock_exchange.py
+++ template-method/stock_exchange.py
@@ -1,16 +1,56 @@
 # -*- coding: UTF-8 -*-
+from abc import ABCMeta, abstractmethod
+
+class template_evaluate:
+    __metaclass__ = ABCMeta
+
+    def evaluate(self,wallet):
+        if self.special_offer(wallet):
+            self.max_evaluate(wallet)
+        else:
+            self.min_evaluate(wallet)
+    
+    @abstractmethod
+    def special_offer(self,wallet):
+        pass
+
+    @abstractmethod
+    def max_evaluate(self,wallet):
+        pass
+
+    @abstractmethod
+    def min_evaluate(self,wallet):
+        pass
 
 
-class twenty_percent:
+class twenty_percent(template_evaluate):
 
-    def evaluate(self, wallet):
-        return wallet.amount * 1.2
+    def special_offer(self, wallet):
+        if wallet.amount > 1000:
+            return True
+        else:
+            return False
+
+    def max_evaluate(self, wallet):
+        return wallet.amount * 1.25
+
+    def min_evaluate(self, wallet):
+        return wallet.amount * 1.20
 
 
-class ten_percent:
+class ten_percent(template_evaluate):
 
-    def evaluate(self, wallet):
-        return wallet.amount * 1.1
+    def special_offer(self, wallet):
+        if wallet.stocks >= 3:
+            return True
+        else:
+            return False
+
+    def max_evaluate(self,wallet):
+        return wallet.amount * 1.12
+
+    def min_evaluate(self,wallet):
+        return wallet.amount * 1.10
 
 
 class five_percent:
